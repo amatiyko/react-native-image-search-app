6851a99c404b55d8522190c589929a6f56c65890
amatiyko <andrew.matiyko@gmail.com>
2018-05-26 23:24:32 +0300

    add search btn

--- src/screens/DisplayScreen.js
+++ src/screens/DisplayScreen.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button 
+} from 'react-native';
 
 export default class DisplayScreen extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Display Screen</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+  });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View, 
+    Text, 
+    Button, 
+    StyleSheet 
+} from 'react-native';
 
 export default class SearchScreen extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Search Screen</Text>
+                <Button
+                    title='Search'
+                    onPress={()=>this.props.navigation.navigate('Display')}
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button :{
+        marginTop: 20
+    }
+});
